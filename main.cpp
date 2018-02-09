99f1a131f35ff3cd3f606d64ab6f9397e50ee9c8
aalhadbah <aalhadbah@mail.csuchico.edu>
2018-02-08 20:27:55 -0800

    Final work

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
-//Author:
+//Author: abdulaziz alhadbah
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
+    
+     char AlphaLetter;
+
 
   cout<<"What character do you want to know about?\n";
+cin>> AlphaLetter;
+          
+          
+  if(AlphaLetter>='A' && AlphaLetter<='Z')
+ {
+ cout<<AlphaLetter<<" is an upper case letter!\n";
+  }
 
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
 
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+ else if(AlphaLetter>='a' && AlphaLetter<='z')
+{
+  cout<<AlphaLetter<<" is a lower case letter!\n";
+}
+ 
 
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+else{
+  cout<<AlphaLetter<<" ?! Pssh. What are you talking about?\n";
   
+}
+
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)AlphaLetter<<endl;
 
   return 0;
 }
